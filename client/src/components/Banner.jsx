bb66cf8edcefbb12afa4eff2f265deef08c61404
Denis Kartachov <kartachovd@gmail.com>
2022-05-10 22:56:01 -0400

    add client

---
+++ client/src/components/Banner.jsx
@@ -0,0 +1,9 @@
+const Banner = () => {
+    return (
+        <div style={{backgroundColor: "black"}}>
+            <div className="someshop">some shop.</div>
+        </div>
+    );
+}
+
+export default Banner;
---
+++ client/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const getProducts = async () => {
+  const res = await fetch('http://localhost:8000/products', {
+    method: 'GET',
+  });
+
+  const data = await res.json();
+
+  console.log(data);
+}
+
+getProducts();
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
